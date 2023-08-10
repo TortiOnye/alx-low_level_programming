e0570142f868709b97ac7d92820f792ec9586e85
TortiOnye <tortijudu12@gmail.com>
2023-08-10 16:48:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,5 @@
 *return: always 0 (success)
 */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);}
